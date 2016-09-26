05d0667ac58f67b0999bcbcde177a7048f74a103
Jaiswal <sumijaiswal@lm-sjn-00876060.paypalcorp.com>
2016-09-26 14:08:17 -0700

    solved 2 problems

---
+++ src/main/java/com/sumit/ds/leetcode/LeetCode344.java
@@ -0,0 +1,24 @@
+package com.sumit.ds.leetcode;
+
+/**
+ * https://leetcode.com/problems/reverse-string/
+ * @author sumijaiswal
+ *
+ */
+public class LeetCode344 {
+	public static String reverseString(String s) {
+        //String reverse = "";
+        StringBuilder sb = new StringBuilder();
+        if(s!=null){
+            for(int i=s.length()-1;i>=0;i--){
+                //reverse= reverse+s.charAt(i);
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(LeetCode344.reverseString("hello"));
+	}
+}
---
+++ src/main/java/com/sumit/ds/leetcode/LeetCode345.java
@@ -0,0 +1,112 @@
+package com.sumit.ds.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Reverse Vowels of a String
+ * https://leetcode.com/problems/reverse-vowels-of-a-string/
+ * @author sumijaiswal
+ *
+ */
+public class LeetCode345 {
+	public static String reverseVowels(String s) {
+		String returnString = null;
+		if (s != null && s.length() > 1) {
+			char[] sarr = s.toCharArray();
+			int tempIndex = 0;
+			char vowelFound = ' ';
+			List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+			for (int i = sarr.length - 1; i >= 0; i--) {
+				if (vowels.contains(sarr[i])) {
+					if (vowelFound == ' ') {
+						vowelFound = sarr[i];
+						tempIndex = i;
+					} else {
+						char temp = sarr[i];
+						// tempIndex = i;
+						sarr[i] = vowelFound;
+						vowelFound = temp;
+					}
+				}
+			}
+			if (vowelFound != ' ')
+				sarr[tempIndex] = vowelFound;
+			returnString = new String(sarr);
+		} else
+			returnString = s;
+
+		return returnString;
+	}
+
+	public static String reverseVowels2(String s) {
+		String returnStr = null;
+		if(s != null && s.length() > 1){
+			List<Character> vowels = Arrays.asList('a','e','i','o','u','A','E','I','O','U');
+			int start = 0;
+			int end = s.length() -1;
+			char [] input = s.toCharArray();
+			while(start < end){
+				while(start < end && !vowels.contains(input[start])){
+					start++;
+				}
+				while(start < end && !vowels.contains(input[end])){
+					end--;
+				}
+				swap(input,start,end);
+				start++;
+				end--;
+			}
+			returnStr = new String(input);
+			
+		}else{
+			returnStr = s;
+		}
+			
+		
+		return returnStr;
+	}
+
+	
+
+	private static void swap(char[] input, int start, int end) {
+		char tmp = input[start];
+		input[start] = input[end];
+		input[end] = tmp;
+		
+	}
+
+	public static String reverseVowels1(String s) {
+		if (s == null || s.length() == 0)
+			return s;
+		String vowels = "aeiouAEIOU";
+		char[] chars = s.toCharArray();
+		int start = 0;
+		int end = s.length() - 1;
+		while (start < end) {
+
+			while (start < end && !vowels.contains(chars[start] + "")) {
+				start++;
+			}
+
+			while (start < end && !vowels.contains(chars[end] + "")) {
+				end--;
+			}
+
+			char temp = chars[start];
+			chars[start] = chars[end];
+			chars[end] = temp;
+
+			start++;
+			end--;
+		}
+		return new String(chars);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(LeetCode345.reverseVowels1("helo let"));
+		System.out.println(LeetCode345.reverseVowels1("race car"));
+		System.out.println(LeetCode345.reverseVowels2("race car"));
+
+	}
+}
